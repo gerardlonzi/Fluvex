31f9802ece6a2715c9e6403605f936456ef7d84a
gerardlonzi <lonzigerard@gmail.com>
2026-03-11 23:33:22 +0100

    feat(ui) : modificatiion du logo de fluvex

--- src/components/layout/DashboardHeader.tsx
+++ src/components/layout/DashboardHeader.tsx
@@ -8,7 +8,7 @@ export function DashboardHeader() {
   return (
     <div className="flex flex-wrap items-center justify-between gap-4 mb-6">
         <div className="md:hidden  flex items-center justify-center shrink-0">
-          <Image src='/mini-logo.png'  alt='mini logo' width={60} height={60} />
+          <Image src='/icon-logo.png'  alt='mini logo' width={60} height={60} />
         </div>
       <Breadcrumb />
       <NotificationBell />
